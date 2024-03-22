2cb9bbabeb4181a05b10b141a3048f6b5b528a4f
Lu Shen <lushen@client-10-228-117-85.tamulink.tamu.edu>
2024-03-22 10:37:09 -0500

    remove make_gui

--- NGDEEP_NIRISS/NGDEEP_check_results.py
+++ NGDEEP_NIRISS/NGDEEP_check_results.py
@@ -164,226 +164,226 @@ def New_Window2(oned, zgrizli):
     button.pack(side=tk.BOTTOM)
    
    
-def Make_Gui(grizli_id, exist_flags, progress, oned, mag, z=[0, 0], path="Extractions_peak1"):
+# def Make_Gui(grizli_id, exist_flags, progress, oned, mag, z=[0, 0], path="Extractions_peak1"):
     
-    if np.isnan(exist_flags[0]):
-        exist_flags = [-1,-1,-1,-1, '', 0, '']
+    # if np.isnan(exist_flags[0]):
+    #     exist_flags = [-1,-1,-1,-1, '', 0, '']
         
-    # ORIGINAL_DPI = 72.05403377110693
-    # def get_dpi():
-    #     screen = tk.Tk()
-    #     current_dpi = screen.winfo_fpixels('1i')
-    #     screen.destroy()
-    #     print(current_dpi)
-    #     return current_dpi
-    # SCALE = get_dpi()/ORIGINAL_DPI
-    # def scaled(original_width):
-    #     return round(original_width * SCALE)
+    # # ORIGINAL_DPI = 72.05403377110693
+    # # def get_dpi():
+    # #     screen = tk.Tk()
+    # #     current_dpi = screen.winfo_fpixels('1i')
+    # #     screen.destroy()
+    # #     print(current_dpi)
+    # #     return current_dpi
+    # # SCALE = get_dpi()/ORIGINAL_DPI
+    # # def scaled(original_width):
+    # #     return round(original_width * SCALE)
 
-    root = tk.Tk()
-    #root.geometry(f'{scaled(1350)}x{scaled(850)}') 
-    root.geometry('1350x850')
-    root.title("Main window")
-    OPTIONS = ["    ", "Great", "Good", "Unclear", "Bad"]
+    # root = tk.Tk()
+    # #root.geometry(f'{scaled(1350)}x{scaled(850)}') 
+    # root.geometry('1350x850')
+    # root.title("Main window")
+    # OPTIONS = ["    ", "Great", "Good", "Unclear", "Bad"]
     
-    ### spectra quality and fitting quality
-    label = tk.Label(root, text='spectra quality:', font=fnt.Font(size = 16), anchor="w", justify="left")
-    label.place(x=450, y=10)
+    # ### spectra quality and fitting quality
+    # label = tk.Label(root, text='spectra quality:', font=fnt.Font(size = 16), anchor="w", justify="left")
+    # label.place(x=450, y=10)
     
-    var0 = tk.StringVar()
-    var0.set(OPTIONS[int(exist_flags[0]+1)])
-    #inputtxt = tk.Entry(root, textvariable=var0, width = 5, font=fnt.Font(size = 16))
-    w0 = tk.OptionMenu(root, var0, *OPTIONS)
-    w0.place(x=580, y=10) 
+    # var0 = tk.StringVar()
+    # var0.set(OPTIONS[int(exist_flags[0]+1)])
+    # #inputtxt = tk.Entry(root, textvariable=var0, width = 5, font=fnt.Font(size = 16))
+    # w0 = tk.OptionMenu(root, var0, *OPTIONS)
+    # w0.place(x=580, y=10) 
 
-    label = tk.Label(root, text='spectra fitting:', font=fnt.Font(size = 16), anchor="w", justify="left")
-    label.place(x=450, y=40)
+    # label = tk.Label(root, text='spectra fitting:', font=fnt.Font(size = 16), anchor="w", justify="left")
+    # label.place(x=450, y=40)
 
-    var1 = tk.StringVar()
-    var1.set(OPTIONS[int(exist_flags[1]+1)])
-    inputtxt = tk.Entry(root, textvariable=var1, width = 5, font=fnt.Font(size = 16))
-    w1 = tk.OptionMenu(root, var1, *OPTIONS)
-    w1.place(x=580, y=40) 
+    # var1 = tk.StringVar()
+    # var1.set(OPTIONS[int(exist_flags[1]+1)])
+    # inputtxt = tk.Entry(root, textvariable=var1, width = 5, font=fnt.Font(size = 16))
+    # w1 = tk.OptionMenu(root, var1, *OPTIONS)
+    # w1.place(x=580, y=40) 
     
-    label = tk.Label(root, text='phot fitting:', font=fnt.Font(size = 16), anchor="w", justify="left")
-    label.place(x=450, y=70)
+    # label = tk.Label(root, text='phot fitting:', font=fnt.Font(size = 16), anchor="w", justify="left")
+    # label.place(x=450, y=70)
     
-    var2 = tk.StringVar()
-    var2.set(OPTIONS[int(exist_flags[2]+1)])
-    #inputtxt = tk.Entry(root, textvariable=var3, width = 5, font=fnt.Font(size = 16), )
-    w2 = tk.OptionMenu(root, var2, *OPTIONS)
-    w2.place(x=580, y=70) 
+    # var2 = tk.StringVar()
+    # var2.set(OPTIONS[int(exist_flags[2]+1)])
+    # #inputtxt = tk.Entry(root, textvariable=var3, width = 5, font=fnt.Font(size = 16), )
+    # w2 = tk.OptionMenu(root, var2, *OPTIONS)
+    # w2.place(x=580, y=70) 
     
-    label = tk.Label(root, text='spec+phot fitting:', font=fnt.Font(size = 16), anchor="w", justify="left")
-    label.place(x=450, y=100)
+    # label = tk.Label(root, text='spec+phot fitting:', font=fnt.Font(size = 16), anchor="w", justify="left")
+    # label.place(x=450, y=100)
     
-    var3 = tk.StringVar()
-    var3.set(OPTIONS[int(exist_flags[3]+1)])
-    #inputtxt = tk.Entry(root, textvariable=var3, width = 5, font=fnt.Font(size = 16), )
-    w3 = tk.OptionMenu(root, var3, *OPTIONS)
-    w3.place(x=580, y=100) 
-    #inputtxt.place(x=580, y=70) 
+    # var3 = tk.StringVar()
+    # var3.set(OPTIONS[int(exist_flags[3]+1)])
+    # #inputtxt = tk.Entry(root, textvariable=var3, width = 5, font=fnt.Font(size = 16), )
+    # w3 = tk.OptionMenu(root, var3, *OPTIONS)
+    # w3.place(x=580, y=100) 
+    # #inputtxt.place(x=580, y=70) 
 
-    label = tk.Label(root, text='Comments:', font=fnt.Font(size = 16), anchor="w", justify="left")
-    label.place(x=450, y=130)
+    # label = tk.Label(root, text='Comments:', font=fnt.Font(size = 16), anchor="w", justify="left")
+    # label.place(x=450, y=130)
     
-    var4 = tk.StringVar()
-    inputtxt = tk.Entry(root, textvariable=var4, width = 15, font=fnt.Font(size=16),)
-    inputtxt.insert(0, exist_flags[4]) 
-    inputtxt.place(x=540, y=130) 
-    
-    
-    ### refit option
-    label = tk.Label(root, text='Refit option [comments format]:', font=fnt.Font(size = 16),  anchor="w", justify="left")
-    label.place(x=800, y=10)
-    
-    varr1 = tk.IntVar()
-    chk1 = tk.Checkbutton(root, text='Incorrect scale on spectra vs. photometry',  font=fnt.Font(size = 16), variable=varr1)
-    if exist_flags[5]//1000 == 1:
-        chk1.select()
-        exist_flags[5] = exist_flags[5]-1000
-    chk1.place(x=800, y=30)
-    
-    varr2 = tk.IntVar()
-    chk2 = tk.Checkbutton(root, text='clip bad photometric points [obs wavelength in um]',  font=fnt.Font(size = 16), variable=varr2)
-    if exist_flags[5]//100 == 1:
-        chk2.select()
-        exist_flags[5] = exist_flags[5]-100
-    chk2.place(x=800, y=53)
-    
-    varr3 = tk.IntVar()
-    chk3 = tk.Checkbutton(root, text='refit with different redshift range [zmin zmax]',  font=fnt.Font(size = 16), variable=varr3)
-    if exist_flags[5]//10 == 1:
-        chk3.select()
-        exist_flags[5] = exist_flags[5]-10
-    chk3.place(x=800, y=76)
-    
-    varr4 = tk.IntVar()
-    chk4 = tk.Checkbutton(root, text='refit [write reason in comment]',  font=fnt.Font(size = 16), variable=varr4)
-    if exist_flags[5]//1 == 1:
-        chk4.select()
-    chk4.place(x=800, y=101)
-    
-    label = tk.Label(root, text='Refit comments:', font=fnt.Font(size = 16), anchor="w", justify="left")
-    label.place(x=800, y=130)
-    
-    varr5 = tk.StringVar()
-    inputtxt = tk.Entry(root, textvariable=varr5, width = 15, font=fnt.Font(size=16))
-    inputtxt.insert(0, exist_flags[6]) 
-    inputtxt.place(x=930, y=130) 
+    # var4 = tk.StringVar()
+    # inputtxt = tk.Entry(root, textvariable=var4, width = 15, font=fnt.Font(size=16),)
+    # inputtxt.insert(0, exist_flags[4]) 
+    # inputtxt.place(x=540, y=130) 
     
     
-    label = tk.Label(root, text='Checking N = %i (total %i): \n'%(progress[0], progress[1]) 
-                     + 'Grizli id: ' + grizli_id 
-                     + ' (%.1f mag)'%mag 
-                     + '\nGrizli fitted to z = %.2f [green in ds9]'%z[0] 
-                     + '\nC20 zphot = %.2f [orange in ds9]'%z[1], 
-                     font=fnt.Font(size = 18), anchor="w", justify="left")
-    label.place(x=20, y=10)
+    # ### refit option
+    # label = tk.Label(root, text='Refit option [comments format]:', font=fnt.Font(size = 16),  anchor="w", justify="left")
+    # label.place(x=800, y=10)
+    
+    # varr1 = tk.IntVar()
+    # chk1 = tk.Checkbutton(root, text='Incorrect scale on spectra vs. photometry',  font=fnt.Font(size = 16), variable=varr1)
+    # if exist_flags[5]//1000 == 1:
+    #     chk1.select()
+    #     exist_flags[5] = exist_flags[5]-1000
+    # chk1.place(x=800, y=30)
+    
+    # varr2 = tk.IntVar()
+    # chk2 = tk.Checkbutton(root, text='clip bad photometric points [obs wavelength in um]',  font=fnt.Font(size = 16), variable=varr2)
+    # if exist_flags[5]//100 == 1:
+    #     chk2.select()
+    #     exist_flags[5] = exist_flags[5]-100
+    # chk2.place(x=800, y=53)
+    
+    # varr3 = tk.IntVar()
+    # chk3 = tk.Checkbutton(root, text='refit with different redshift range [zmin zmax]',  font=fnt.Font(size = 16), variable=varr3)
+    # if exist_flags[5]//10 == 1:
+    #     chk3.select()
+    #     exist_flags[5] = exist_flags[5]-10
+    # chk3.place(x=800, y=76)
+    
+    # varr4 = tk.IntVar()
+    # chk4 = tk.Checkbutton(root, text='refit [write reason in comment]',  font=fnt.Font(size = 16), variable=varr4)
+    # if exist_flags[5]//1 == 1:
+    #     chk4.select()
+    # chk4.place(x=800, y=101)
+    
+    # label = tk.Label(root, text='Refit comments:', font=fnt.Font(size = 16), anchor="w", justify="left")
+    # label.place(x=800, y=130)
+    
+    # varr5 = tk.StringVar()
+    # inputtxt = tk.Entry(root, textvariable=varr5, width = 15, font=fnt.Font(size=16))
+    # inputtxt.insert(0, exist_flags[6]) 
+    # inputtxt.place(x=930, y=130) 
+    
+    
+    # label = tk.Label(root, text='Checking N = %i (total %i): \n'%(progress[0], progress[1]) 
+    #                  + 'Grizli id: ' + grizli_id 
+    #                  + ' (%.1f mag)'%mag 
+    #                  + '\nGrizli fitted to z = %.2f [green in ds9]'%z[0] 
+    #                  + '\nC20 zphot = %.2f [orange in ds9]'%z[1], 
+    #                  font=fnt.Font(size = 18), anchor="w", justify="left")
+    # label.place(x=20, y=10)
     
 
-    path_img = path+"%s.%s.png"%(grizli_id, 'full')
-    img2 = ImageTk.PhotoImage(Image.open(path_img))
-    tk.Label(root, image=img2).place(x=20, y=175)
+    # path_img = path+"%s.%s.png"%(grizli_id, 'full')
+    # img2 = ImageTk.PhotoImage(Image.open(path_img))
+    # tk.Label(root, image=img2).place(x=20, y=175)
 
-    path_img = path+"%s.%s.png"%(grizli_id, 'stack')
-    img3 = ImageTk.PhotoImage(Image.open(path_img))
-    tk.Label(root, image=img3).place(x=840, y=175)
+    # path_img = path+"%s.%s.png"%(grizli_id, 'stack')
+    # img3 = ImageTk.PhotoImage(Image.open(path_img))
+    # tk.Label(root, image=img3).place(x=840, y=175)
 
-    path_img = path+"%s.%s.png"%(grizli_id, 'sed')
-    img1 = ImageTk.PhotoImage(Image.open(path_img))
-    tk.Label(root, image=img1).place(x=20, y=530)
+    # path_img = path+"%s.%s.png"%(grizli_id, 'sed')
+    # img1 = ImageTk.PhotoImage(Image.open(path_img))
+    # tk.Label(root, image=img1).place(x=20, y=530)
 
-    # Add information about wavelengths of strong features 
-    line_list_gui = np.array(['OII', 'OII', 'NeIII', 'Hg', 'Hb','OIII', 'OIII', 'Ha', 'PaB', 'PaA'])
-    line_lambda_gui = np.array([3726.0, 3728.8, 3868, 4361, 4861.3, 4958.9, 5006.8, 6562.8, 12822, 18756])
-    OIIobs = (line_lambda_gui[0] + line_lambda_gui[1])/2*(1+z[0])
-    NeIIIobs = line_lambda_gui[2]*(1+z[0])
-    Hgobs = line_lambda_gui[3]*(1+z[0])
-    Hbobs = line_lambda_gui[4]*(1+z[0])
-    OIIIbobs = line_lambda_gui[5]*(1+z[0])
-    OIIIrobs = line_lambda_gui[6]*(1+z[0])
-    Haobs = line_lambda_gui[7]*(1+z[0])
-    Pbobs = line_lambda_gui[8]*(1+z[0])
-    Paobs = line_lambda_gui[9]*(1+z[0])
+    # # Add information about wavelengths of strong features 
+    # line_list_gui = np.array(['OII', 'OII', 'NeIII', 'Hg', 'Hb','OIII', 'OIII', 'Ha', 'PaB', 'PaA'])
+    # line_lambda_gui = np.array([3726.0, 3728.8, 3868, 4361, 4861.3, 4958.9, 5006.8, 6562.8, 12822, 18756])
+    # OIIobs = (line_lambda_gui[0] + line_lambda_gui[1])/2*(1+z[0])
+    # NeIIIobs = line_lambda_gui[2]*(1+z[0])
+    # Hgobs = line_lambda_gui[3]*(1+z[0])
+    # Hbobs = line_lambda_gui[4]*(1+z[0])
+    # OIIIbobs = line_lambda_gui[5]*(1+z[0])
+    # OIIIrobs = line_lambda_gui[6]*(1+z[0])
+    # Haobs = line_lambda_gui[7]*(1+z[0])
+    # Pbobs = line_lambda_gui[8]*(1+z[0])
+    # Paobs = line_lambda_gui[9]*(1+z[0])
 
-    label = tk.Label(root, text='lambda_obs of emission \nfeatures at z_Grizli:', font=fnt.Font(size = 18), anchor="w", justify="left")
-    label.place(x=1130, y=530)
+    # label = tk.Label(root, text='lambda_obs of emission \nfeatures at z_Grizli:', font=fnt.Font(size = 18), anchor="w", justify="left")
+    # label.place(x=1130, y=530)
 
-    label = tk.Label(root, text='[OII]: %.1f'%OIIobs + ' A', font=fnt.Font(size = 17), anchor="w", justify="left", fg='#6C2DC7', bg='#FFFFFF')
-    label.place(x=1130, y=580)
+    # label = tk.Label(root, text='[OII]: %.1f'%OIIobs + ' A', font=fnt.Font(size = 17), anchor="w", justify="left", fg='#6C2DC7', bg='#FFFFFF')
+    # label.place(x=1130, y=580)
 
-    label = tk.Label(root, text='[NeIII]: %.1f'%NeIIIobs + ' A', font=fnt.Font(size = 17), anchor="w", justify="left", fg='#5865F2', bg='#FFFFFF')
-    label.place(x=1130, y=605)
+    # label = tk.Label(root, text='[NeIII]: %.1f'%NeIIIobs + ' A', font=fnt.Font(size = 17), anchor="w", justify="left", fg='#5865F2', bg='#FFFFFF')
+    # label.place(x=1130, y=605)
 
-    label = tk.Label(root, text='Hg: %.1f'%Hgobs + ' A', font=fnt.Font(size = 17), anchor="w", justify="left", fg='#16E2F5', bg='#FFFFFF')
-    label.place(x=1130, y=630)
+    # label = tk.Label(root, text='Hg: %.1f'%Hgobs + ' A', font=fnt.Font(size = 17), anchor="w", justify="left", fg='#16E2F5', bg='#FFFFFF')
+    # label.place(x=1130, y=630)
 
-    label = tk.Label(root, text='Hb: %.1f'%Hbobs + ' A', font=fnt.Font(size = 17), anchor="w", justify="left", fg='#1B8A6B',bg='#FFFFFF')
-    label.place(x=1130, y=655)
+    # label = tk.Label(root, text='Hb: %.1f'%Hbobs + ' A', font=fnt.Font(size = 17), anchor="w", justify="left", fg='#1B8A6B',bg='#FFFFFF')
+    # label.place(x=1130, y=655)
 
-    label = tk.Label(root, text='[OIII]: %.1f'%OIIIbobs + ' A', font=fnt.Font(size = 17), anchor="w", justify="left", fg='#16F529', bg='#FFFFFF')
-    label.place(x=1130, y=680)
+    # label = tk.Label(root, text='[OIII]: %.1f'%OIIIbobs + ' A', font=fnt.Font(size = 17), anchor="w", justify="left", fg='#16F529', bg='#FFFFFF')
+    # label.place(x=1130, y=680)
 
-    label = tk.Label(root, text='[OIII]: %.1f'%OIIIrobs + ' A', font=fnt.Font(size = 17), anchor="w", justify="left", fg='#E9AB17', bg='#FFFFFF')
-    label.place(x=1130, y=705)
+    # label = tk.Label(root, text='[OIII]: %.1f'%OIIIrobs + ' A', font=fnt.Font(size = 17), anchor="w", justify="left", fg='#E9AB17', bg='#FFFFFF')
+    # label.place(x=1130, y=705)
 
-    label = tk.Label(root, text='Ha: %.1f'%Haobs + ' A', font=fnt.Font(size = 17), anchor="w", justify="left", fg='#FF8C00', bg='#FFFFFF')
-    label.place(x=1130, y=730)
+    # label = tk.Label(root, text='Ha: %.1f'%Haobs + ' A', font=fnt.Font(size = 17), anchor="w", justify="left", fg='#FF8C00', bg='#FFFFFF')
+    # label.place(x=1130, y=730)
 
-    label = tk.Label(root, text='Pb: %.1f'%Pbobs + ' A', font=fnt.Font(size = 17), anchor="w", justify="left", fg='#E42217', bg='#FFFFFF')
-    label.place(x=1130, y=755)
+    # label = tk.Label(root, text='Pb: %.1f'%Pbobs + ' A', font=fnt.Font(size = 17), anchor="w", justify="left", fg='#E42217', bg='#FFFFFF')
+    # label.place(x=1130, y=755)
 
-    label = tk.Label(root, text='Pa: %.1f'%Paobs + ' A', font=fnt.Font(size = 17), anchor="w", justify="left", fg='#800517', bg='#FFFFFF')
-    label.place(x=1130, y=780)
+    # label = tk.Label(root, text='Pa: %.1f'%Paobs + ' A', font=fnt.Font(size = 17), anchor="w", justify="left", fg='#800517', bg='#FFFFFF')
+    # label.place(x=1130, y=780)
     
-    ### Add 1d spectra window 
-    button = tk.Button(root, text="plot 1D spectra", bg='White', fg='dark orange', font=fnt.Font(size=20), 
-                              command=lambda: New_Window(oned))
-    button.place(x=20, y=100)
+    # ### Add 1d spectra window 
+    # button = tk.Button(root, text="plot 1D spectra", bg='White', fg='dark orange', font=fnt.Font(size=20), 
+    #                           command=lambda: New_Window(oned))
+    # button.place(x=20, y=100)
     
-    button = tk.Button(root, text="plot 1D spectra (smoothed)", bg='White', fg='dark orange', font=fnt.Font(size=20), 
-                              command=lambda: New_Window2(oned))
-    button.place(x=20, y=140)
+    # button = tk.Button(root, text="plot 1D spectra (smoothed)", bg='White', fg='dark orange', font=fnt.Font(size=20), 
+    #                           command=lambda: New_Window2(oned))
+    # button.place(x=20, y=140)
     
     
-    ### Add exit and next button 
-    def Close(): 
-        np.savetxt('.exit_tmp', [1])
-        root.destroy() 
+    # ### Add exit and next button 
+    # def Close(): 
+    #     np.savetxt('.exit_tmp', [1])
+    #     root.destroy() 
         
-    def Next(): 
-        np.savetxt('.exit_tmp', [0])
-        root.destroy() 
+    # def Next(): 
+    #     np.savetxt('.exit_tmp', [0])
+    #     root.destroy() 
         
-    exit_button = tk.Button(root, text="Exit", command=Close, font=fnt.Font(size=20))  #command=lambda m="It is an apple": which_button(m)
-    exit_button.place(x=1250, y=60)
+    # exit_button = tk.Button(root, text="Exit", command=Close, font=fnt.Font(size=20))  #command=lambda m="It is an apple": which_button(m)
+    # exit_button.place(x=1250, y=60)
     
-    next_button = tk.Button(root, text="Next", command=Next,  font=fnt.Font(size=20))  #command=lambda m="It is an apple": which_button(m)
-    next_button.place(x=1250, y=20)
-    ###
+    # next_button = tk.Button(root, text="Next", command=Next,  font=fnt.Font(size=20))  #command=lambda m="It is an apple": which_button(m)
+    # next_button.place(x=1250, y=20)
+    # ###
     
-    root.mainloop()
+    # root.mainloop()
     
-    exit_param = np.loadtxt('.exit_tmp')
+    # exit_param = np.loadtxt('.exit_tmp')
     
-    spectra_flag = var0.get()
-    spec_fit_flag = var1.get()
-    phot_fit_flag = var2.get()
-    specphot_fit_flag = var3.get()
+    # spectra_flag = var0.get()
+    # spec_fit_flag = var1.get()
+    # phot_fit_flag = var2.get()
+    # specphot_fit_flag = var3.get()
     
     
-    refit_flag = 0
-    if varr1.get() == 1:
-        refit_flag += 1000
-    if varr2.get() == 1:
-        refit_flag += 100
-    if varr3.get() == 1:
-        refit_flag += 10
-    if varr4.get() == 1:
-        refit_flag += 1
+    # refit_flag = 0
+    # if varr1.get() == 1:
+    #     refit_flag += 1000
+    # if varr2.get() == 1:
+    #     refit_flag += 100
+    # if varr3.get() == 1:
+    #     refit_flag += 10
+    # if varr4.get() == 1:
+    #     refit_flag += 1
     
-    return [spectra_flag, spec_fit_flag, phot_fit_flag, specphot_fit_flag], var4.get(), refit_flag, varr5.get(), exit_param
+    # return [spectra_flag, spec_fit_flag, phot_fit_flag, specphot_fit_flag], var4.get(), refit_flag, varr5.get(), exit_param
 
 def Make_Gui2(grizli_id, exist_flags, progress, oned, mag, z=[0, 0], path="Extractions_peak1"):
     
